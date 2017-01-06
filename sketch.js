06a2cda89dec73a4548fe597a8f76faf8c093b6c
Matt Panzer <mattpanzer@Matts-MBP.home>
2017-01-05 23:08:27 -0500

    Added diacritics filter, modified color ranges.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var words; // An array to store all words to analyze
 var rhymes = []; // Stores all of the words' rhymes (not really used)
 var groups = []; // Stores the words in groups based on their rhyme
 var colors = []; // The colors that words will be (coorespond to the groups)
+var bcolor;
 var index = 0; // The current word we're looking at
 
 // A function to replace all instances of a string within another string
@@ -12,6 +13,98 @@ String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
+
+var diacriticsMap = {
+  '\u00C0': 'A',  // À => A
+  '\u00C1': 'A',   // Á => A
+  '\u00C2': 'A',   // Â => A
+  '\u00C3': 'A',   // Ã => A
+  '\u00C4': 'A',   // Ä => A
+  '\u00C5': 'A',   // Å => A
+  '\u00C6': 'AE', // Æ => AE
+  '\u00C7': 'C',   // Ç => C
+  '\u00C8': 'E',   // È => E
+  '\u00C9': 'E',   // É => E
+  '\u00CA': 'E',   // Ê => E
+  '\u00CB': 'E',   // Ë => E
+  '\u00CC': 'I',   // Ì => I
+  '\u00CD': 'I',   // Í => I
+  '\u00CE': 'I',   // Î => I
+  '\u00CF': 'I',   // Ï => I
+  '\u0132': 'IJ', // Ĳ => IJ
+  '\u00D0': 'D',   // Ð => D
+  '\u00D1': 'N',   // Ñ => N
+  '\u00D2': 'O',   // Ò => O
+  '\u00D3': 'O',   // Ó => O
+  '\u00D4': 'O',   // Ô => O
+  '\u00D5': 'O',   // Õ => O
+  '\u00D6': 'O',   // Ö => O
+  '\u00D8': 'O',   // Ø => O
+  '\u0152': 'OE', // Œ => OE
+  '\u00DE': 'TH', // Þ => TH
+  '\u00D9': 'U',   // Ù => U
+  '\u00DA': 'U',   // Ú => U
+  '\u00DB': 'U',   // Û => U
+  '\u00DC': 'U',   // Ü => U
+  '\u00DD': 'Y',   // Ý => Y
+  '\u0178': 'Y',   // Ÿ => Y
+  '\u00E0': 'a',   // à => a
+  '\u00E1': 'a',   // á => a
+  '\u00E2': 'a',   // â => a
+  '\u00E3': 'a',   // ã => a
+  '\u00E4': 'a',   // ä => a
+  '\u00E5': 'a',   // å => a
+  '\u00E6': 'ae', // æ => ae
+  '\u00E7': 'c',   // ç => c
+  '\u00E8': 'e',   // è => e
+  '\u00E9': 'e',   // é => e
+  '\u00EA': 'e',   // ê => e
+  '\u00EB': 'e',   // ë => e
+  '\u00EC': 'i',   // ì => i
+  '\u00ED': 'i',   // í => i
+  '\u00EE': 'i',   // î => i
+  '\u00EF': 'i',   // ï => i
+  '\u0133': 'ij', // ĳ => ij
+  '\u00F0': 'd',   // ð => d
+  '\u00F1': 'n',   // ñ => n
+  '\u00F2': 'o',   // ò => o
+  '\u00F3': 'o',   // ó => o
+  '\u00F4': 'o',   // ô => o
+  '\u00F5': 'o',   // õ => o
+  '\u00F6': 'o',   // ö => o
+  '\u00F8': 'o',   // ø => o
+  '\u0153': 'oe', // œ => oe
+  '\u00DF': 'ss', // ß => ss
+  '\u00FE': 'th', // þ => th
+  '\u00F9': 'u',   // ù => u
+  '\u00FA': 'u',   // ú => u
+  '\u00FB': 'u',   // û => u
+  '\u00FC': 'u',   // ü => u
+  '\u00FD': 'y',   // ý => y
+  '\u00FF': 'y',   // ÿ => y
+  '\uFB00': 'ff', // ﬀ => ff
+  '\uFB01': 'fi',   // ﬁ => fi
+  '\uFB02': 'fl', // ﬂ => fl
+  '\uFB03': 'ffi',  // ﬃ => ffi
+  '\uFB04': 'ffl',  // ﬄ => ffl
+  '\uFB05': 'ft', // ﬅ => ft
+  '\uFB06': 'st'  // ﬆ => st
+}; // Map diacritics to regular characters
+
+// A function to strip diactitics out of a string
+function replaceDiacritics(str) {
+  var returnStr = '';
+  if(str) {
+    for (var i = 0; i < str.length; i++) {
+      if (diacriticsMap[str[i]]) {
+        returnStr += diacriticsMap[str[i]];
+      } else {
+        returnStr += str[i];
+      }
+    }
+  }
+  return returnStr;
+}
 
 // Setting up the page
 function setup() {
@@ -32,7 +125,9 @@ function newText() {
   rhymes = [];
   groups = [];
   colors = [];
-  words = s.split(/(\W+)/);
+  bcolor;
+  words = replaceDiacritics(s);
+  words = words.split(/(\W+)/);
   words = words.join("`").toLowerCase().replaceAll("`'`","").split("`");
   analyzeWords();
 }
@@ -46,7 +141,6 @@ function showRhymesOnly() {
     if (!/\W+/.test(word)) {
       for (var g = 0; g < groups.length; g++) {
         var group = groups[g];
-        console.log(group);
         if ((group.indexOf(word) != -1) && (group.length > 1)) {
           var ci = groups.indexOf(group);
           span.style('background-color', colors[ci]);
@@ -76,9 +170,8 @@ function analyzeWords() {
         cRhymes.push(words[index]);
         rhymes.push(cRhymes);
 
-        // If no rhymes found, it's a random color
-        var wColor = color(random(100,255), random(100,255), random(100,255));
         var foundGroup = false;
+        var wColor = color(random(100,240), random(100,240), random(100,240));
 
         // Going backwards, loop through all groups
         for (var j = groups.length-1; j >= 0; j--) {
@@ -92,6 +185,7 @@ function analyzeWords() {
         }
         if (!foundGroup) {
           // We didn't find any rhymes, make it its own group and own color
+          usecolor = bcolor;
           groups.push([words[index]])
           colors.push(wColor);
         }
